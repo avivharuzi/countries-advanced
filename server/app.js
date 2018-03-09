9327f5425b96455911eba15718d07d06c70ce72f
avivharuzi <avivharuzi@gmail.com>
2018-03-09 23:47:50 +0200

    More changes and added dist folder

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const path = require('path');
 
 const countriesRoute = require('./routes/countries.route');
 const favoritesRoute = require('./routes/favorites.route');
 const authRoute = require('./routes/auth.route');
 
-const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger('dev'));
@@ -16,17 +16,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
+app.use(express.static(path.join(__dirname, '../dist')));
+
 app.use('/', authRoute);
 app.use('/api/countries', countriesRoute);
 app.use('/api/favorites', favoritesRoute);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
+
+module.exports = app;
