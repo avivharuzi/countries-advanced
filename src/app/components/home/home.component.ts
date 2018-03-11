13a3ec731886329dea97bdc262c412a44f1a04af
avivharuzi <avivharuzi@gmail.com>
2018-03-11 15:13:02 +0200

    Image fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BASE_IMAGES_PATH } from '../../constants/urls';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public bg: string;
 
-  constructor() { }
+  constructor() {
+    this.bg = null;
+  }
 
   ngOnInit() {
+    this.bg = `${BASE_IMAGES_PATH}/main/bg.jpg`;
   }
 
 }
--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -2,3 +2,5 @@ const BASE_URL = 'http://localhost:3000';
 
 export const BASE_COUNTRIES_URL: string = `${BASE_URL}/api/countries`;
 export const BASE_FAVORITES_URL: string = `${BASE_URL}/api/favorites`;
+
+export const BASE_IMAGES_PATH: string = `${BASE_URL}/images`;
